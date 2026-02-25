cb2ee48d9c6fdabe4acc004ab19bbfc889faf2db
Chipatenii <innocentmanda70@gmail.com>
2026-02-25 15:49:38 +0200

    feat: Implement initial website structure including homepage, products, and contact pages.

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize input
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    // Recipient email (Change this to Sekoma's sales email)
+    $recipient = "sales@sekomaenergy.com";
+    $subject = "New Website Inquiry from $name";
+
+    // Email headers
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
